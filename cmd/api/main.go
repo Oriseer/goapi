9adb4f9de47f177d52501df7ac22b001a29872eb
Oriseer <parianjohnmichael@gmail.com>
2024-07-19 13:26:59 +0800

    updated folders

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,13 +9,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func main() [
+func main() {
 	log.SetReportCaller(true)
 
 	var r *chi.Mux = chi.NewRouter()
-	handlers.handler(r)
+	handlers.Handler(r)
 
 	fmt.Println("\n Starting New GO API service...")
 
 	err := http.ListenAndServe("localhost:8080", r)
+	if err != nil {
+		log.Error(err)
+	}
 }
